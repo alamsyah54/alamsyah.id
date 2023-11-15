2a98cc8082de5b000e4a0a0e1450316abd4946a3
asdasdwasd6699 <aldialamsyah911@gmail.com>
2023-11-15 17:54:06 +0700

    tailwindconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,15 @@
-import type { Config } from "tailwindcss"
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
 
-const config: Config = {
-    darkMode: ["class"],
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
     theme: {
-        screens: {
-            sm: "640px",
-            // => @media (min-width: 640px) { ... }
 
-            md: "768px",
-            // => @media (min-width: 768px) { ... }
-
-            lg: "1024px",
-            // => @media (min-width: 1024px) { ... }
-
-            xl: "1280px",
-            // => @media (min-width: 1280px) { ... }
-
-            "2xl": "1536px",
-            // => @media (min-width: 1536px) { ... }
-        },
         extend: {
             colors: {
                 dark: {
@@ -45,4 +31,3 @@ const config: Config = {
     },
     plugins: [],
 }
-export default config
