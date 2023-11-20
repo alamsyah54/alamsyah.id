2c029113ea49b0e2ceaeb2c50d860e9a2a8aa5fa
asdasdwasd6699 <aldialamsyah911@gmail.com>
2023-11-20 07:01:28 +0700

    y

--- components/shared/ProductsCarousel.tsx
+++ components/shared/ProductsCarousel.tsx
@@ -34,7 +34,7 @@ const ProductsCarousel = () => {
 
     return (
         <AnimatePresence mode='wait'>
-            <div className='relative md:hidden lg:hidden w-full'>
+            <div className='w-full lg:hidden justify-center items-center flex flex-col'>
                 {/* <Image
                 alt='ALAMSYAH.ID'
                 className='min-h-fit bg-gray-300 w-full bg-center bg-cover duration-500 transition-transform'
@@ -47,7 +47,7 @@ const ProductsCarousel = () => {
                     whileInView={{ opacity: 1, y: 0, x: 0 }}
                     transition={{ duration: 1 }}
                     viewport={{ once: false }}
-                    className='flex flex-col px-5 rounded-3xl duration-700 shadow-lg bg-gray-100 dark:bg-dark-600 border-2 border-gray-50 dark:border-dark-500 dark:shadow-black shadow-black/30 items-center '
+                    className='flex flex-col px-5 rounded-3xl w-fit duration-700 shadow-lg bg-gray-100 dark:bg-dark-600 border-2 border-gray-50 dark:border-dark-500 dark:shadow-black shadow-black/30 items-center '
                 >
                     <h1
                         className={`text-4xl py-5 duration-700 w-full text-left border-b-[1px] dark:border-white/30 border-black/30 text-transparent bg-gradient-to-r dark:from-white from-black bg-clip-text ${roboto.className}`}
@@ -98,13 +98,13 @@ const ProductsCarousel = () => {
                 <div className='flex justify-center mt-5'>
                     {Products.map((product, slideIndex) => (
                         <motion.div
-                            initial={{ opacity: 0, y: -70, x: 0 }}
+                            initial={{ opacity: 0, y: -50, x: 0 }}
                             whileInView={{ opacity: 1, y: 0, x: 0 }}
                             viewport={{ once: false }}
-                            transition={{ duration: 2 }}
+                            transition={{ duration: 1 }}
                             key={slideIndex}
                             onClick={() => goToSlide(slideIndex)}
-                            className='px-3 duration-700 py-1 -z-20 rounded-full m-3 cursor-pointer border-2 border-white dark:border-dark-600 bg-gradient-to-bl from-gray-50 from-15% via-gray-100 to-gray-200 dark:from-dark-500 dark:from-15% dark:via-dark-600 via-40% dark:to-dark-700 to-90% '
+                            className='px-3 duration-700 py-1 rounded-full m-3 cursor-pointer border-2 border-white dark:border-dark-600 bg-gradient-to-bl from-gray-50 from-15% via-gray-100 to-gray-200 dark:from-dark-500 dark:from-15% dark:via-dark-600 via-40% dark:to-dark-700 to-90% '
                         >
                             <h4
                                 className={`flex items-center px-3 py-1 ${roboto.className}`}
