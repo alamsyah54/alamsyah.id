bec6452deccdfd2cbb7924b090218ce9e2937b38
asdasdwasd6699 <aldialamsyah911@gmail.com>
2023-11-24 15:37:22 +0700

    njir

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             <DetailSection />
             <Rules />
             <Testimoni />
-            <div className='w-full h-2 fixed bottom-0 bg-green-500 sm:bg-yellow-500 md:bg-orange-500 lg:bg-red-500' />
+            {/* <div className='w-full h-2 fixed bottom-0 bg-green-500 sm:bg-yellow-500 md:bg-orange-500 lg:bg-red-500' /> */}
         </main>
     )
 }
