a1f6d77e1616fdf901f7665890f2141cc4457ec7
asdasdwasd6699 <aldialamsyah911@gmail.com>
2023-12-20 19:21:01 +0700

    head title

---
+++ app/(root)/products/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Products",
+    description:
+        "ALAMSYAH.ID your trusted Netflix Premium Accounts Seller. high-quality, secured, and exclusively for Netflix enthusiasts.",
+    icons: {
+        icon: [
+            {
+                url: "/icons/AHeaderDark.webp",
+                media: "(prefers-color-scheme: light)",
+            },
+            {
+                url: "/icons/AHeaderLight.webp",
+                media: "(prefers-color-scheme: dark)",
+            },
+        ],
+    },
+    keywords:
+        "netflix, premium, 4k, uhd, full garansi, netflix account, netflix premium uhd, netflix murah, netflix shared, netflix private, netflix terpercaya, murah, terpercaya, film terbaru, alamsyah.id, alamsyah, bergaransi",
+}
