ceb5699b3f8dda62f98e1fb809bd85c9b1b2b0a3
asdasdwasd6699 <aldialamsyah911@gmail.com>
2024-01-10 22:09:47 +0700

    y

--- app/(root)/accounts/page.tsx
+++ app/(root)/accounts/page.tsx
@@ -13,7 +13,7 @@ const roboto = Roboto({
 const page = async () => {
     const { Data, totalPrivateAccounts, totalSharedAccounts, totalccounts } =
         await fetchData()
-    setInterval(fetchData, 3000)
+    setInterval(fetchData, 61000)
     console.log("///////////////////////////////////")
     console.log(Data)
     console.log("///////////////////////////////////")
