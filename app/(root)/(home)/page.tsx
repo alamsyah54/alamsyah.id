a0adb89e79d3aadb5db5a4e9e265c1131bf36d93
asdasdwasd6699 <aldialamsyah911@gmail.com>
2024-01-13 05:34:42 +0700

    y

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,61 +1,24 @@
-"use client"
-import React, { useState, useEffect } from "react"
+import React, { Suspense } from "react"
 import DetailSection from "@/components/DetailSection"
 import HeroSection from "@/components/HeroSection"
 import AdsSection from "@/components/AdsSection"
 import PromoSection from "@/components/PromoSection"
 import Rules from "@/components/Rules"
 import Testimoni from "@/components/Testimoni"
-import Image from "next/image"
+import Loading from "@/components/shared/Loading"
 
 export default function Home() {
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        const delay = setTimeout(() => {
-            setLoading(false)
-        }, 1111) // Set the duration in milliseconds (2 seconds in this example)
-
-        return () => clearTimeout(delay)
-    }, [])
-
-    const Loading = () => {
-        return (
-            <div className='flex flex-col w-screen h-screen justify-center items-center gap-10'>
-                <Image
-                    width={100}
-                    height={100}
-                    src='/images/AFooterDark.png'
-                    alt='Loading...'
-                    className='hidden dark:block duration-700'
-                />
-                <Image
-                    width={100}
-                    height={100}
-                    src='/images/AFooterLight.png'
-                    alt='Loading...'
-                    className='dark:hidden duration-700'
-                />
-                <p className='text-lg text-transparent bg-gradient-to-bl dark:from-white from-black bg-clip-text cursor-default select-none'>
-                    Loading...
-                </p>
-            </div>
-        )
-    }
     return (
-        <>
-            {loading ? (
-                <Loading />
-            ) : (
-                <main className='flex flex-col h-fit items-center justify-center'>
-                    <HeroSection />
-                    {/* <PromoSection /> */}
-                    <AdsSection />
-                    <DetailSection />
-                    <Rules />
-                    <Testimoni />
-                </main>
-            )}
-        </>
+        <Suspense fallback={<Loading />}>
+            <main className='flex flex-col h-fit items-center justify-center'>
+                {/* <Loading /> */}
+                <HeroSection />
+                {/* <PromoSection /> */}
+                <AdsSection />
+                <DetailSection />
+                <Rules />
+                <Testimoni />
+            </main>
+        </Suspense>
     )
 }
--- components/shared/Loading.tsx
+++ components/shared/Loading.tsx
@@ -2,24 +2,26 @@ import Image from "next/image"
 
 const Loading = () => {
     return (
-        <div className='flex flex-col w-screen h-screen justify-center -mt-24 items-center gap-2'>
-            <Image
-                width={100}
-                height={100}
-                src='/images/AFooterDark.png'
-                alt='Loading...'
-                className='hidden dark:block duration-700'
-            />
-            <Image
-                width={100}
-                height={100}
-                src='/images/AFooterLight.png'
-                alt='Loading...'
-                className='dark:hidden duration-700'
-            />
-            <p className='text-lg text-transparent bg-gradient-to-bl dark:from-white from-black bg-clip-text cursor-default select-none'>
-                Loading...
-            </p>
+        <div className='absolute z-[999] top-0 left-0'>
+            <div className='flex flex-col w-screen h-screen justify-center bg-gray-200 dark:bg-dark-700 items-center gap-2'>
+                <Image
+                    width={100}
+                    height={100}
+                    src='/images/AFooterDark.png'
+                    alt='Loading...'
+                    className='hidden dark:block duration-700'
+                />
+                <Image
+                    width={100}
+                    height={100}
+                    src='/images/AFooterLight.png'
+                    alt='Loading...'
+                    className='dark:hidden duration-700'
+                />
+                <p className='font-conthrax text-lg text-transparent bg-gradient-to-bl dark:from-white from-black bg-clip-text cursor-default select-none'>
+                    Loading...
+                </p>
+            </div>
         </div>
     )
 }
