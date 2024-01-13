9fec3928223facc783c1020220b6cba38c033c1f
asdasdwasd6699 <aldialamsyah911@gmail.com>
2024-01-13 07:11:43 +0700

    y

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,21 +1,24 @@
 import Footer from "@/components/shared/Footer"
+import Loading from "@/components/shared/Loading"
 import Navbar from "@/components/shared/Navbar"
 
-import React from "react"
+import React, { Suspense } from "react"
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <main className='relative '>
-            <Navbar />
-            <div className='flex'>
-                <div className='flex min-h-screen flex-1 flex-col pt-24'>
-                    <div className='mx-auto w-full'>{children}</div>
+        <Suspense fallback={<Loading />}>
+            <main className='relative '>
+                <Navbar />
+                <div className='flex'>
+                    <div className='flex min-h-screen flex-1 flex-col pt-24'>
+                        <div className='mx-auto w-full'>{children}</div>
+                    </div>
                 </div>
-            </div>
-            <Footer />
-            <div className='galaxy-background opacity-75 -z-50' />
-            {/* <div className='w-full h-2 fixed bottom-0 bg-green-500 sm:bg-yellow-500 md:bg-orange-500 lg:bg-red-500' /> */}
-        </main>
+                <Footer />
+                <div className='galaxy-background opacity-75 -z-50' />
+                {/* <div className='w-full h-2 fixed bottom-0 bg-green-500 sm:bg-yellow-500 md:bg-orange-500 lg:bg-red-500' /> */}
+            </main>
+        </Suspense>
     )
 }
 
