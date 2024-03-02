370bfc4dfff351e080cb4b11a189d3cef80b0276
Alamsyah <aldialamsyah0054@gmail.com>
2024-03-03 04:14:25 +0700

    ya

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             <main className='flex flex-col h-fit items-center justify-center'>
                 <HeroSection />
                 <Accounts />
-                <PromoSection />
+                {/* <PromoSection /> */}
                 <AdsSection />
                 <DetailSection />
                 <Rules />
